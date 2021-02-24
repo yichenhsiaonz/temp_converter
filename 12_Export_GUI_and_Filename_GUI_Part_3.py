b1ff41b85ec114f568aa27cfe531e89d521252d1
hsiaoy71950 <51727091+hsiaoy71950@users.noreply.github.com>
2021-02-24 13:20:11 +1300

    yes haha

--- 12_Export_GUI_and_Filename_GUI_Part_3.py
+++ 12_Export_GUI_and_Filename_GUI_Part_3.py
@@ -275,6 +275,47 @@ class History:
         partner.history_button.config(state=NORMAL)
         self.history_box.destroy()
 
+
+class Export:
+    def __init__(self, partner):
+        background = "orange"
+
+        # disable help button
+        partner.help_button.config(state=DISABLED)
+
+        # Sets up child window (ie) help box
+        self.help_box = Toplevel()
+
+        # If users press cross at top, closes help and 'releases' help button
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+
+        # Set up GUI Frame
+        self.help_frame = Frame(self.help_box, width=300, bg=background)
+        self.help_frame.grid()
+        # Set up Help heading (row 0)
+        self.help_label = Label(self.help_frame, text="Help / Instructions",
+                                font=("Arial", "14", "bold"),
+                                bg=background,
+                                padx=10, pady=10)
+        self.help_label.grid(row=0)
+
+        # Help text (label, row 1)
+        self.help_text = Label(self.help_frame, text="",
+                               justify=LEFT, width=40,
+                               bg=background, wrap=250, )
+        self.help_text.grid(row=1)
+
+        # Dismiss button (row 2)
+        self.dismiss_btn = Button(self.help_frame, text="Dismiss",
+                                  width=10, bg="orange", font="arial 10 bold",
+                                  command=partial(self.close_help, partner))
+        self.dismiss_btn.grid(row=2, pady=10)
+
+    def close_help(self, partner):
+        # Put help button back to normal...
+        partner.help_button.config(state=NORMAL)
+        self.help_box.destroy()
+
 # main routine
 
 calculation_history = []
